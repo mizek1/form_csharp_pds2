b54336eabde047ae5fe4c65463cf075f7fc37657
Danilo Alves <daniloalves@aluno.fapce.edu.br>
2020-04-25 11:18:47 -0300

    renomeando componentes

--- form_csharp/Form1.Designer.cs
+++ form_csharp/Form1.Designer.cs
@@ -28,36 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btEntrar = new System.Windows.Forms.Button();
+            this.btSair = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.ctEmail = new System.Windows.Forms.TextBox();
+            this.ctSenha = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // btEntrar
             // 
-            this.button1.Location = new System.Drawing.Point(205, 140);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Entrar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btEntrar.Location = new System.Drawing.Point(205, 140);
+            this.btEntrar.Name = "btEntrar";
+            this.btEntrar.Size = new System.Drawing.Size(75, 23);
+            this.btEntrar.TabIndex = 0;
+            this.btEntrar.Text = "Entrar";
+            this.btEntrar.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btSair
             // 
-            this.button2.Location = new System.Drawing.Point(298, 140);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Sair";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btSair.Location = new System.Drawing.Point(298, 140);
+            this.btSair.Name = "btSair";
+            this.btSair.Size = new System.Drawing.Size(75, 23);
+            this.btSair.TabIndex = 1;
+            this.btSair.Text = "Sair";
+            this.btSair.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 57);
+            this.label1.Location = new System.Drawing.Point(16, 52);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(38, 13);
             this.label1.TabIndex = 2;
@@ -66,37 +66,37 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 92);
+            this.label2.Location = new System.Drawing.Point(13, 95);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(41, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Senha:";
             // 
-            // textBox1
+            // ctEmail
             // 
-            this.textBox1.Location = new System.Drawing.Point(81, 49);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(292, 20);
-            this.textBox1.TabIndex = 4;
+            this.ctEmail.Location = new System.Drawing.Point(81, 49);
+            this.ctEmail.Name = "ctEmail";
+            this.ctEmail.Size = new System.Drawing.Size(292, 20);
+            this.ctEmail.TabIndex = 4;
             // 
-            // textBox2
+            // ctSenha
             // 
-            this.textBox2.Location = new System.Drawing.Point(81, 92);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(292, 20);
-            this.textBox2.TabIndex = 5;
+            this.ctSenha.Location = new System.Drawing.Point(81, 92);
+            this.ctSenha.Name = "ctSenha";
+            this.ctSenha.Size = new System.Drawing.Size(292, 20);
+            this.ctSenha.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(399, 188);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ctSenha);
+            this.Controls.Add(this.ctEmail);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btSair);
+            this.Controls.Add(this.btEntrar);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -106,12 +106,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btEntrar;
+        private System.Windows.Forms.Button btSair;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox ctEmail;
+        private System.Windows.Forms.TextBox ctSenha;
     }
 }
 
